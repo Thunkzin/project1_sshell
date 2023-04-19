ff1879a3093d6bf3cbeafe5bbf1529bf268426d4
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-04-19 15:49:52 -0700

    Add files via upload

--- sshell.c
+++ sshell.c
@@ -2,18 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define CMDLINE_MAX 512
 #define ARGUMENT_MAX 16
 #define TOKEN_LENGTH_MAX 32
 #define PIPE_NUMBER_MAX 3
 
-int main(void)
-{
+
+int system_sshell(char **args){
+        pid_t pid = fork();
+        if(pid == 0){
+                execvp(args[0], args);
+                perror("execvp");
+                exit(1);
+        }else if(pid > 0){
+                int status;
+                waitpid(pid, &status, 0);
+        }else{
+                perror("fork");
+                exit(1);
+        }
+        return 0;
+}
+
+char** parsing_command_to_argument(char cmd[CMDLINE_MAX], char cmd_copy[CMDLINE_MAX]){
+        char *token;
+        char **args = malloc(ARGUMENT_MAX);
+        int position = 0;
+        strcpy(cmd_copy, cmd);
+
+        // get the first token
+        token = strtok(cmd_copy, " ");
+        while(token != NULL) {
+                // Store the rest of the tokens into args
+                args[position] = token;
+                position += 1;
+                token = strtok(NULL,  " ");
+        }
+        if(position >= ARGUMENT_MAX){
+                fprintf(stderr,"Error: too many process arguments\n");
+                return NULL;
+        }
+        return(args);
+}
+
+int main(void){
         char cmd[CMDLINE_MAX];
-        /* char args[TOKEN_LENGTH_MAX];  */
+        char cmd_copy[CMDLINE_MAX];
+        char **args;
 
         while (1) {
                 char *nl;
@@ -24,8 +62,32 @@ int main(void)
                 fflush(stdout);
 
                 /* Get command line */
-                fgets(cmd, CMDLINE_MAX, stdin);
+                fgets(cmd, CMDLINE_MAX, stdin); 
+                /* Remove the last \0 */
+                cmd[strlen(cmd)-1] = '\0';
+               
+                /* Test
+                printf("cmd = :%s \n", cmd);
+                */
 
+                /* Parse the cmd into **args[] */
+                args = parsing_command_to_argument(cmd, cmd_copy);
+               
+                /* Test
+                printf("cmd = :%s \n", cmd);
+                */
+
+                /*
+                printf for test
+                printf("args[0]: %s\n",args[0]);
+                printf("args[1]: %s\n",args[1]);
+                printf("args[2]: %s\n",args[2]);
+                */
+
+                /* Redo the loop if the error is received */
+                if (args[0]== NULL){
+                        continue;
+                }
                 /* Print command line if stdin is not provided by terminal */
                 if (!isatty(STDIN_FILENO)) {
                         printf("%s", cmd);
@@ -38,81 +100,28 @@ int main(void)
                         *nl = '\0';
 
                 /* Builtin command */
-                //exit
-                if (!strcmp(cmd, "exit")) {
+                if (!strcmp(args[0], "exit")) {
                         fprintf(stderr, "Bye...\n");
-                        printf("+ completed %s [0]\n", cmd);
+                        printf("+ completed %s [0]\n", args[0]);
                         break;
                 }
-                //pwd
-                if (!strcmp(cmd, "pwd")) {
+                if (!strcmp(args[0], "pwd")) {
                         char cwd[CMDLINE_MAX];
                         getcwd(cwd, sizeof(cwd));
                         printf("%s\n",cwd);
-                        printf("+ completed %s [0]\n", cmd);
+                        printf("+ completed %s [0]\n", args[0]);
                         continue;
                 }
-                //cd
-                if (!strcmp(cmd, "cd")) {
-                        printf("+ completed %s [0]\n", cmd);
+                if (!strcmp(args[0], "cd")) {
+                        chdir(args[1]);
+                        printf("+ completed %s [0]\n", args[0]);
                         continue;
                 }
                 /* Regular command */
                 // Parsing the commend before 
-                retval = system(cmd);
-                printf("+ completed %s [%d]\n", cmd, retval);
-                fprintf(stderr, "Return status value for '%s': %d\n", cmd, retval);     
+                retval = system_sshell(args);
+                printf("+ completed %s [%d]\n", cmd, retval);   
                         
         }
         return EXIT_SUCCESS;
 }
-
-int system_sshell(char *cmd, char *args[]){
-        pid_t pid = fork();
-        if(pid == 0){
-                execvp(cmd, args);
-                perror("execvp");
-                exit(1);
-        }else if(pid > 0){
-                int status;
-                waitpid(pid, &status, 0);
-                printf("+ completed %s [%d]\n", cmd, status);
-
-        }else{
-                perror("fork");
-                exit(1);
-        }
-        return 0;
-}
-
-char parsing_command(char *cmd){
-        const char s[2] = " ";
-        char *token;
-        /* get the first token */
-        token = strtok(cmd, s);
-        /* walk through other tokens */
-        while( token != NULL ) {
-                printf( " %s\n", token );
-                token = strtok(NULL, s);
-        }
-        return(0);
-}
-
-/*
-
-char reading_command(char cmd){
-
-}
-
-void redirection(){
-
-}
-
-void pipe(){
-       // program1 > file && file > program2
-}
-
-// 1 Parsing command
-
-// 
-*/
