f89e6f745cc8c0c4dd8487cf22c478957e4f31e3
Hsienchi-Chiu <131100559+Hsienchi-Chiu@users.noreply.github.com>
2023-04-20 22:29:52 -0700

    Update sshell.c

--- sshell.c
+++ sshell.c
@@ -7,24 +7,12 @@
 #include <errno.h>
 #include <dirent.h>
 #include <fcntl.h>
+
 #define CMDLINE_MAX 512
 #define ARGUMENT_MAX 16
 #define TOKEN_LENGTH_MAX 32
 #define PIPE_NUMBER_MAX 3
-
-int redirection_stdout(char **file) {
-        int fd = open(file, O_WRONLY);
-        if (fd == -1) {
-                /* Error */
-                return -1;
-        }
-        if (dup2(fd, STDOUT_FILENO) == -1) {
-                close(fd);
-                return -1;
-        }
-        close(fd);
-        return 0;
-}
+#define MAX_ENVIRONMENT_NUMBER 25
 
 int system_sshell(char **args){
         pid_t pid;
@@ -42,21 +30,19 @@ int system_sshell(char **args){
         }
         return WEXITSTATUS(status);
 }
-
-char** parsing_command_to_argument(char cmd[CMDLINE_MAX],char cmd_copy[CMDLINE_MAX] , char to_be_parsed[2]){
+char** parsing_command_to_argument(char cmd[CMDLINE_MAX],char cmd_copy[CMDLINE_MAX] , char to_be_parsed[]){
         #define SIGN_TO_BE_PARSED " \0"
         char *token;
-        char **args = malloc(ARGUMENT_MAX);
         char **token_array = malloc(ARGUMENT_MAX);
         int position = 0;
         strcpy(cmd_copy, cmd);
         cmd_copy[strlen(cmd_copy)-1] = '\0';
+
        /* Get the first Token , program, args[0]*/
         token = strtok(cmd_copy, to_be_parsed);
-
+        
         while(token != NULL) {
                 // Store the rest of the tokens into args[]
-                args[position] = token;
                 token_array[position] = token;
                 position += 1;
                 token = strtok(NULL, to_be_parsed);
@@ -64,16 +50,10 @@ char** parsing_command_to_argument(char cmd[CMDLINE_MAX],char cmd_copy[CMDLINE_M
         if(position >= ARGUMENT_MAX){
                 fprintf(stderr,"Error: too many process arguments\n");
                 /* Set args[0] to be NULL so the shell will start a new loop */
-                args[0] = NULL;
                 token_array[0] = NULL;
         }
-        return(args);
         return(token_array);
 }
-
-// void redirection(char** before_redirection, char** after_redirection){
-
-// }
 
 int redirection(char **file) {
         int fd = open(*file, O_WRONLY);
@@ -87,11 +67,11 @@ int redirection(char **file) {
 }
 
 void pipeline(char *cmd) {
-    char *commands = cmd[CMDLINE_MAX];
     char *commands = &cmd[CMDLINE_MAX];
     int num_pipes = strlen(commands) - 1;
     int pipe_fds[PIPE_NUMBER_MAX][2];
     pid_t child_pids[CMDLINE_MAX];
+
     // Create the pipes
     for (int i = 0; i < num_pipes; i++) {
         if (pipe(pipe_fds[i]) == -1) {
@@ -100,6 +80,7 @@ void pipeline(char *cmd) {
             exit(EXIT_FAILURE);
         }
     }
+
     // Fork the child processes
     for (int i = 0; i <= num_pipes; i++) {
         if ((child_pids[i] = fork()) == -1) {
@@ -107,6 +88,7 @@ void pipeline(char *cmd) {
             fprintf(stderr, "Error: missing command\n");
             exit(EXIT_FAILURE);
         }
+
         if (child_pids[i] == 0) { // Child process
             // Connect to the previous pipe (if there is one)
             if (i > 0) {
@@ -116,6 +98,7 @@ void pipeline(char *cmd) {
                 }
                 close(pipe_fds[i-1][0]);
             }
+
             // Connect to the next pipe (if there is one)
             if (i < num_pipes) {
                 if (dup2(pipe_fds[i][1], STDOUT_FILENO) == -1) {
@@ -124,11 +107,11 @@ void pipeline(char *cmd) {
                 }
                 close(pipe_fds[i][1]);
             }
+
             // Parse the command and execute it
             char *args[256];
             char *token;
             int j = 0;
-            token = strtok(commands[i], " \t\n");
             token = strtok(&commands[i], " \t\n");
             while (token != NULL) {
                 args[j] = token;
@@ -141,11 +124,13 @@ void pipeline(char *cmd) {
             exit(EXIT_FAILURE);
         }
     }
+
     // Close all the pipes
     for (int i = 0; i < num_pipes; i++) {
         close(pipe_fds[i][0]);
         close(pipe_fds[i][1]);
     }
+
     // Wait for all the child processes to terminate
     int status;
     for (int i = 0; i <= num_pipes; i++) {
@@ -162,12 +147,9 @@ int main(void){
         char cmd[CMDLINE_MAX];
         char cmd_copy[CMDLINE_MAX];
         char **args = malloc(ARGUMENT_MAX);
-        // char **redirection_args;
-        //
-        // char **right_args;
-        // char **left_args_parsed_white_space;
-        // char **right_args_parsed_white_space;
-
+        // char **alphabet_set = malloc(MAX_ENVIRONMENT_NUMBER);
+        // char **stored_data_set = malloc(MAX_ENVIRONMENT_NUMBER);
+        // int key = 0;
         // char **left_args = malloc(ARGUMENT_MAX);
         while (1) {
                 char *nl;
@@ -178,51 +160,23 @@ int main(void){
                 /* Get command line */
                 fgets(cmd, CMDLINE_MAX, stdin);
 
-
-                /* Remove the last \0 */
-                // cmd[strlen(cmd)-1] = '\0';
-                // if(strchr(cmd, '>') != NULL){
-                //         /* Command contains redirection */
-                //         printf("aaaaa");
-                //         redirection_args = parsing_command_to_argument(cmd, cmd_copy, ">");
-                //         printf("bbbbb");
-                //         right_args = *redirection_args[1];
-                //         left_args_parsed_white_space = parsing_command_to_argument(redirection_args[0], cmd_copy, " ");
-                //         right_args_parsed_white_space = parsing_command_to_argumen(&right_args, cmd_copy, " ");
-                //         system_sshell(left_args_parsed_white_space);
-
-                // }else{
-                //         /* Command withouth redirection */
-                // }
                 if(strchr(cmd, '>') != NULL){
                         args = parsing_command_to_argument(cmd, cmd_copy, ">");
-                        char **left_args = malloc(ARGUMENT_MAX);
-                        left_args = parsing_command_to_argument(args[0], cmd_copy, " ");
-                        printf("leftargs[0]: %s\n",left_args[0]);
-                        printf("leftargs[1]: %s\n",left_args[1]);
-                        printf("leftargs[2]: %s\n",left_args[2]);
-                        printf("leftargs[3]: %s\n",left_args[3]);
-                        printf("....\n");
-                }else{
-                args = parsing_command_to_argument(cmd, cmd_copy, " ");
-
+                        if(args[1] == NULL){
+                                fprintf(stderr,"Error: no output file\n");
+                                continue;
+                        }
+                        if(!opendir(args[1])){
+                                fprintf(stderr,"Error: cannot open output file\n");
+                                continue;
+                        }
                         // printf("leftargs[0]: %s\n",left_args[0]);
                         // printf("leftargs[1]: %s\n",left_args[1]);
                 }else{
                         args = parsing_command_to_argument(cmd, cmd_copy, " ");
                 }
 
-
                 /* Parse the cmd into **args[] */
-
-
-                printf("args[0]: %s\n",args[0]);
-                printf("args[1]: %s\n",args[1]);
-                printf("args[2]: %s\n",args[2]);
-                printf("args[3]: %s\n",args[3]);
-                printf("args[4]: %s\n",args[4]);
-                printf("args[5]: %s\n",args[5]);
-                printf("args[6]: %s\n",args[6]);
                 // printf("args[0]: %s\n",args[0]);
                 // printf("args[1]: %s\n",args[1]);
                 // printf("args[2]: %s\n",args[2]);
@@ -233,19 +187,23 @@ int main(void){
 
 
                 /* Redo the loop if no input */
-                if (!strcmp(args[0], "\n")){
-                        free(args);
+                if (args[0] ==NULL){
                         continue;
                 }
+
+
                 /* Print command line if stdin is not provided by terminal */
                 if (!isatty(STDIN_FILENO)) {
                         printf("%s", cmd);
                         fflush(stdout);
                 }
+
                 /* Remove trailing newline from command line */
                 nl = strchr(cmd, '\n');
                 if (nl)
                         *nl = '\0';
+
+
                 /* Builtin command */
                 if (!strcmp(args[0], "exit")) {
                         fprintf(stderr, "Bye...\n");
@@ -260,6 +218,33 @@ int main(void){
                         free(args);
                         continue;
                 }
+
+        //        if (!strcmp(args[0], "set")) {
+        //                 /* Store the environment variables and its data into pointers */
+        //                 alphabet_set[key] = args[1];
+        //                 stored_data_set[key] = args[2];
+        //                 key++;
+        //                 continue;
+        //         }                
+        //         if(strchr(cmd, '$') != NULL){
+        //                 args = parsing_command_to_argument(cmd, cmd_copy, " $");
+        //                 /* Use double loop to check if the $alphabet in the input is stored in alphabet_set */
+        //                 for(int count = 0 ; count <= ARGUMENT_MAX; count ++){
+        //                         for(int alphabet_count = 0 ; alphabet_count <= 25 ; alphabet_count ++){
+        //                                 if (!strcmp(args[count], alphabet_set[alphabet_count])) {
+        //                                         args[count] = stored_data_set[alphabet_count];
+        //                                 }               
+        //                         }
+        //                 }
+        //         }
+        //         printf("args[0]: %s\n",args[0]);
+        //         printf("args[1]: %s\n",args[1]);
+        //         printf("args[2]: %s\n",args[2]);
+        //         printf("args[3]: %s\n",args[3]);
+        //         printf("args[4]: %s\n",args[4]);
+        //         printf("args[5]: %s\n",args[5]);
+        //         printf("args[6]: %s\n",args[6]);
+
                 if (!strcmp(args[0], "cd")) {
                         DIR* directory = opendir(args[1]);
                         if (directory) {
@@ -279,7 +264,6 @@ int main(void){
                         fprintf(stderr, "+ completed '%s' [%d]\n", cmd, retval);   
                         free(args);
                 }
-                free(args[0]);
 
         }
 
